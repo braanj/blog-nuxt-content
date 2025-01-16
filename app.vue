d378b35d09c1389151bb847c1e62c23e8423f7b2
braanj <anjar.bra@gmail.com>
2025-01-16 20:16:46 +0100

    Feat: adding index page and a default layout

--- app.vue
+++ app.vue
@@ -1,25 +1,6 @@
-<script setup lang="ts">
-const { t } = useI18n()
-</script>
-
 <template>
-  <div class="centered">
-    <p>{{ t('message') }}</p>
+  <div>
+    <NuxtLoadingIndicator />
+    <NuxtLayout />
   </div>
 </template>
-
-<style>
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-
-.centered {
-  border: 1px solid #aeaeae;
-  height: 100svh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+  <div class="centered">
+    <p>{{ t('message') }}</p>
+  </div>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.centered {
+  border: 1px solid #aeaeae;
+  height: 100svh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
